6e708ed462581addfe06b1db08d4f1036066f252
aBARICHELLO <arturbarichello@hotmail.com>
2017-06-29 15:51:15 -0300

    Fixed button

--- bot.py
+++ bot.py
@@ -7,13 +7,12 @@ updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 job = updater.job_queue
 
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def start(bot, update):
     button = KeyboardButton("Send your location", request_location=True)
-    keyboard = ReplyKeyboardMarkup([][button])
+    keyboard = ReplyKeyboardMarkup([[button]])
     update.message.reply_text("Hi! Press the button to send me your location!", reply_markup=keyboard)
 
 def location(bot, update):
@@ -24,7 +23,7 @@ def my_location(bot, update):
     update.message.reply_text(location)
 
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Send location with /location")
+    bot.send_message(chat_id=update.message.chat_id, text="Start me with /start and send me your location.")
 
 def help(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="useful help text")
