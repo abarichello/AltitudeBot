315c39a3a857fc4bd8a29bbc45e0f26f6142187c
aBARICHELLO <arturbarichello@hotmail.com>
2017-06-29 15:35:26 -0300

    Removed f strings

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+from config import TOKEN
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Job
 import logging
 
-updater = Updater(token='')
+updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 job = updater.job_queue
 
@@ -12,18 +13,15 @@ logger = logging.getLogger(__name__)
 
 def start(bot, update):
     button = KeyboardButton("Send your location", request_location=True)
-    keyboard = ReplyKeyboardMarkup([[button]])
+    keyboard = ReplyKeyboardMarkup([][button])
     update.message.reply_text("Hi! Press the button to send me your location!", reply_markup=keyboard)
 
 def location(bot, update):
     location = update.message.location
-    update.message.reply_text(f"latitude:{location.latitude}, longitude:{location.longitude}")
+    update.message.reply_text("latitude: {}, longitude: {}".format(location.latitude, location.longitude))
 
-def my_location(bot, update):
-    if location is None:
-        update.message.reply_text("Send me your location first!")
-    else:
-        update.message.reply_text(location)
+def my_location(bot, update):    
+    update.message.reply_text(location)
 
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Send location with /location")
